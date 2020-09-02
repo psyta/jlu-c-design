7e3e9b2227cc2c06ebdc9dae41d43988d8b696e6
C-learner678 <55736734+C-learner678@users.noreply.github.com>
2020-09-02 15:27:07 +0800

    结构体设计完成

---
+++ patient.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+struct patient {
+	char name[20];
+	int age;
+	int register_id; //π“∫≈£®≤ª÷ÿ∏¥£©
+};
---
+++ treatment.cpp
@@ -0,0 +1,41 @@
+//钱以分为单位
+
+struct treatment {
+	
+};
+
+struct body_Check { //体检
+	char name[30];
+	int price;
+	struct body_Check * next;
+};
+
+struct medicine { //药品信息
+	char name[30];
+	int unit_Price; //单价
+};
+
+struct medicine_list { //记录全部药品信息
+	struct * medicine[30];
+};
+
+struct used_Medicine { //用药
+	struct medicine * use_m;
+	int amount;
+	struct used_Medicine * next;
+};
+
+struct time { //时间
+	int month;
+	int day;
+	int hour;
+	int minute;
+};
+
+struct live_hospital {
+	struct time in_time; //住院日期
+	struct time out_time; //预计出院日期
+	int predict_days; //预计住院天数
+	int pledge;//剩余押金
+	int cost;//已支付住院费用
+};
