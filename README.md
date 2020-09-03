0e5a9095d36fe6af09e844f0681bfb8f92c1c174
levy pan <levy-pan@outlook.com>
2020-09-03 10:08:23 +0800

    modify

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 jlu-c-design
+
+## 解决 win 代码去到 mac 上的编码错乱
+
+```
+$enca -L chinese -x UTF-8 *.h
+```
--- treatment.h
+++ treatment.h
@@ -1,7 +1,7 @@
 #ifndef TREATMENT_H
 #define TREATMENT_H
 
-//Ç®ÒÔ·ÖÎªµ¥Î»
+//é’±ä»¥åˆ†ä¸ºå•ä½
 
 struct treatment;
 struct body_Check;
@@ -11,7 +11,7 @@ struct used_Medicine;
 struct time;
 struct live_hospital;
 
-struct treatment { //¾ùÖ¸Ïòhead
+struct treatment { //å‡æŒ‡å‘head
 	struct body_Check * bc;
 	int check_total_price;
 	struct used_medicine * um;
@@ -19,29 +19,29 @@ struct treatment { //¾ùÖ¸Ïòhead
 	struct live_hospital * lh;
 };
 
-struct body_Check { //Ìå¼ì
+struct body_Check { //ä½“æ£€
 	char name[30];
 	int price;
 	struct body_Check * next;
 };
 
-struct medicine { //Ò©Æ·ĞÅÏ¢
+struct medicine { //è¯å“ä¿¡æ¯
 	char name[30];
-	int unit_Price; //µ¥¼Û
+	int unit_Price; //å•ä»·
 	struct medicine * next;
 };
 
-struct medicine_list { //¼ÇÂ¼È«²¿Ò©Æ·ĞÅÏ¢
+struct medicine_list { //è®°å½•å…¨éƒ¨è¯å“ä¿¡æ¯
 	struct medicine * head;
 };
 
-struct used_Medicine { //ÓÃÒ©
+struct used_Medicine { //ç”¨è¯
 	struct medicine * use_m;
 	int amount;
 	struct used_Medicine * next;
 };
 
-struct time { //Ê±¼ä
+struct time { //æ—¶é—´
 	int month;
 	int day;
 	int hour;
@@ -49,11 +49,11 @@ struct time { //Ê±¼ä
 };
 
 struct live_hospital {
-	struct time in_time; //×¡ÔºÈÕÆÚ
-	struct time out_time; //Ô¤¼Æ³öÔºÈÕÆÚ
-	int predict_days; //Ô¤¼Æ×¡ÔºÌìÊı
-	int pledge;//Ê£ÓàÑº½ğ
-	int cost;//ÒÑÖ§¸¶×¡Ôº·ÑÓÃ
+	struct time in_time; //ä½é™¢æ—¥æœŸ
+	struct time out_time; //é¢„è®¡å‡ºé™¢æ—¥æœŸ
+	int predict_days; //é¢„è®¡ä½é™¢å¤©æ•°
+	int pledge;//å‰©ä½™æŠ¼é‡‘
+	int cost;//å·²æ”¯ä»˜ä½é™¢è´¹ç”¨
 };
 
 #endif
